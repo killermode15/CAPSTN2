63b66ab1051bb6194496796d65c740fca7bc800b
DESKTOP-M49PEAP\Anton Declaro <declaroanton@gmail.com>
2018-01-07 21:55:41 +0800

    Added le Character Movement (left and right) for xbox controller

--- Capstone2/Assets/Scripts/Placeholder/AbsorbEnergy.cs
+++ Capstone2/Assets/Scripts/Placeholder/AbsorbEnergy.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class AbsorbEnergy : MonoBehaviour
 {
 
-	public List<GameObject> AbsorbableObjects;
-	public bool IsSelectingObject;
-
-	private GameObject currentSelectedObject;
-	private int selectedIndex;
 	// Use this for initialization
 	void Start()
 	{
@@ -19,25 +14,6 @@ public class AbsorbEnergy : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetButton("LeftTrigger") && AbsorbableObjects.Count > 0)
-		{
-			currentSelectedObject = AbsorbableObjects[selectedIndex];
-			if (currentSelectedObject)
-			{
-				if (Input.GetButtonDown("AButton"))
-				{
-					GetComponent<Energy>().AddEnergy(50);
-					AbsorbableObjects.Remove(currentSelectedObject);
-					Destroy(currentSelectedObject);
-					return;
-				}
-				Debug.Log(currentSelectedObject.name);
-			}
-		}
+		
 	}
-
-	//GameObject SelectObject()
-	//{
-
-	//}
 }
---
+++ Capstone2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 6.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	Rigidbody rBody;
+
+	void Start(){
+		rBody = GetComponent<Rigidbody> ();
+		rBody.freezeRotation = true;
+	}
+	void Update(){
+		Move ();
+	}
+
+	void Move(){
+		CharacterController controller = GetComponent<CharacterController> ();
+
+		moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= speed;
+
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
