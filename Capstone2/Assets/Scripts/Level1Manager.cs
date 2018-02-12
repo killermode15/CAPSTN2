ed56298e833e587e207b1af0a8aa89e8239b3fe3
DESKTOP-M49PEAP\Anton Declaro <declaroanton@gmail.com>
2018-02-12 16:01:12 +0800

    Updated the Tutorial and Level1. still tryin out the Pop Up chuchu

---
+++ Capstone2/Assets/Scripts/Level1Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Manager : MonoBehaviour {
+
+	public GameObject CorruptedLevel;
+	public GameObject  CleanLevel;
+	public bool isCorrupted;
+
+	// Use this for initialization
+	void Start () {
+		isCorrupted = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CorruptedLevel.SetActive(isCorrupted);
+		CleanLevel.SetActive(!isCorrupted);
+	}
+}
