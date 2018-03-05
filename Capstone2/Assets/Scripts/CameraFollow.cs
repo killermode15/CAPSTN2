6358ab2bf626e29e2b7a893ab0b30c5fcb3fbbc7
LAPTOP-END1ORH3\seank <seankeiffer@gmail.com>
2018-03-05 20:30:24 +0800

    + Fix camera movement (AGaIN!!!) FINALLLLLLY

--- Capstone2/Assets/Scripts/CameraFollow.cs
+++ Capstone2/Assets/Scripts/CameraFollow.cs
@@ -8,7 +8,8 @@ public class CameraFollow : MonoBehaviour
 	public float OrbitSpeed = 4f;
 	public float FollowSpeed = 4f;
 	public Vector3 Offset;
-	public Vector2 CameraYLimit = new Vector2(23, 35);
+	public Vector2 CameraYPosLimit = new Vector2(1.5f, 7.0f);
+	public Vector2 CameraYRotLimit = new Vector2(23, 35);
 
 	private GameObject player;
 	private GameObject target;
@@ -22,27 +23,54 @@ public class CameraFollow : MonoBehaviour
 
 	private void LateUpdate()
 	{
+
 		float orbitValue = Input.GetAxis("RightStickX");
-		float panValue= Input.GetAxis("RightStickY");
+		float panValue = Input.GetAxis("RightStickY") ;
+
+		if (camOffset.y < CameraYPosLimit.x)
+		{
+			if (panValue < 0)
+			{
+				Debug.Log("Pan value reaches lowest val");
+				panValue = 0;
+			}
+			camOffset.y = CameraYPosLimit.x;
+		}
+		if (camOffset.y > CameraYPosLimit.y)
+		{
+			if (panValue > 0)
+			{
+				Debug.Log("Pan value reaches highest val");
+				panValue = 0;
+			}
+			camOffset.y = CameraYPosLimit.y;
+		}
+
 
 		Quaternion camTurnAngle = Quaternion.AngleAxis(orbitValue * OrbitSpeed, Vector3.up);
 		Quaternion camPanAngle = Quaternion.AngleAxis(panValue * OrbitSpeed, transform.right);
-
 		camOffset = camPanAngle * camTurnAngle * camOffset;
 
 		Vector3 origPos = new Vector3(camOffset.x, camOffset.y, camOffset.z);
 		camOffset += transform.TransformDirection(Offset);
+
+		//Debug.Log(camOffset);
+		camOffset.y = Mathf.Clamp(camOffset.y, CameraYPosLimit.x - 0.01f, CameraYPosLimit.y + 0.01f);
+
 		Vector3 newPos = player.transform.position + camOffset;
 
 		transform.position = Vector3.Slerp(transform.position, newPos, Time.deltaTime * FollowSpeed);
 		transform.LookAt(player.transform);
+
+		Vector3 euler = transform.eulerAngles;
+		euler.x = Mathf.Clamp(euler.x, CameraYRotLimit.x, CameraYRotLimit.y);
+		transform.eulerAngles = euler;
 
 		camOffset = origPos;
 	}
 
 	Vector3 GetDirFromAngle(float angleInDegrees)
 	{
-
 		return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
 	}
 }
--- Capstone2/Assets/Scripts/PlayerScripts/LockOn.cs
+++ Capstone2/Assets/Scripts/PlayerScripts/LockOn.cs
@@ -7,149 +7,226 @@ using UnityEngine.UI;
 public class LockOn : MonoBehaviour
 {
 
-    public GameObject Crosshair;
-    public GameObject currentTarget;
-    public List<GameObject> allEnemies = new List<GameObject>();
-    public List<GameObject> visibleEnemies = new List<GameObject>();
-    bool firstSelecting;
-    bool switchButtonPressed;
-    private int index;
+	public GameObject Crosshair;
+	public GameObject currentTarget;
+	public List<GameObject> allEnemies = new List<GameObject>();
+	public List<GameObject> visibleEnemies = new List<GameObject>();
+	bool firstSelecting;
+	bool switchButtonPressed;
+	private int selectedTarget;
+	private List<GameObject> sortedVisibleEnemies;
 
-    // Use this for initialization
-    void Start()
-    {
-        FindAllEnemies();
-        //allEnemies = GameObject.FindObjectsOfType<Absorbable>().ToList();
+	// Use this for initialization
+	void Start()
+	{
+		FindAllEnemies();
+		//allEnemies = GameObject.FindObjectsOfType<Absorbable>().ToList();
 
-        Crosshair = Instantiate(Crosshair);
-        Crosshair.SetActive(false);
-    }
+		Crosshair = Instantiate(Crosshair);
+		Crosshair.SetActive(false);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        CheckIfInCombat();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		CheckIfInCombat();
+		SortListByDistance();
+		PickTarget();
 
-    void CheckIfInCombat()
-    {
-        if (Input.GetButtonDown("LeftTrigger"))
-            firstSelecting = true;
-        if (Input.GetButton("LeftTrigger"))
-        {
-            CheckForEnemiesInScreen();
-            //FindAllEnemies ();
-        }
-        else if (Input.GetButtonUp("LeftTrigger"))
-        {
-            Crosshair.SetActive(false);
-        }
-    }
+	}
 
-    void PickTarget()
-    {
-        // if nothing to switch to, set Crosshair.SetActive(false);
-        if (firstSelecting)
-        {
-            currentTarget = FindNearestVisibileEnemy();
-            firstSelecting = false;
-        }
-        else if (!firstSelecting)
-        {
-            if (visibleEnemies.Count > 0)
-            {
-                Debug.Log(visibleEnemies.Count);
+	void CheckIfInCombat()
+	{
+		if (Input.GetButtonDown("LeftTrigger"))
+			firstSelecting = true;
+		if (Input.GetButton("LeftTrigger"))
+		{
+			CheckForEnemiesInScreen();
+			//FindAllEnemies ();
+		}
+		else if (Input.GetButtonUp("LeftTrigger"))
+		{
+			Crosshair.SetActive(false);
+		}
+	}
 
-                if (Input.GetButtonDown("R3") && !switchButtonPressed)
-                {
-                    //if(currentTarget == visibleEnemies[visibleEnemies.Count])
-                    switchButtonPressed = true;
-                    index++;
-                    Debug.Log(visibleEnemies.Count);
-                    if (index > visibleEnemies.Count)
-                    {
-                        index = 0;
-                    }
-                    currentTarget = visibleEnemies[index];
-                }
-                else if (Input.GetButtonUp("R3") && switchButtonPressed)
-                {
-                    switchButtonPressed = false;
-                }
-            }
-        }
-        CrosshairLock();
-    }
+	void PickTarget()
+	{
+		// if nothing to switch to, set Crosshair.SetActive(false);
+		if (firstSelecting)
+		{
+			if (sortedVisibleEnemies.Count > 0)
+				currentTarget = sortedVisibleEnemies[0];
+			firstSelecting = false;
+			selectedTarget = 0;
+		}
+		else if (!firstSelecting)
+		{
+			if (sortedVisibleEnemies.Count > 0)
+			{
+				currentTarget = sortedVisibleEnemies[selectedTarget];
 
-    void FindAllEnemies()
-    {
-        State[] allEnemies = FindObjectsOfType(typeof(State)) as State[];
-        foreach (State Enemy in allEnemies)
-        {
-            //visibleEnemies.Add(Enemy);
-            this.allEnemies.Add(Enemy.gameObject);
-        }
-        this.allEnemies = this.allEnemies.Distinct().ToList();
-    }
+				if (Input.GetKeyDown(KeyCode.Joystick1Button11))
+				{
+					Debug.Log("R3 Pressed");
+					selectedTarget++;
+					Debug.Log(visibleEnemies.Count);
+					if (selectedTarget >= visibleEnemies.Count)
+					{
+						selectedTarget = 0;
+					}
+				}
 
-    void CheckForEnemiesInScreen(/*GameObject enemy*/)
-    {
-        visibleEnemies = new List<GameObject>();
-        foreach (GameObject Enemy in allEnemies)
-        {
-            Vector3 screenPoint = Camera.main.WorldToViewportPoint(Enemy.transform.position);
-            bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
-            if (onScreen)
-            {
-                //CrosshairLock();
-                visibleEnemies.Add(Enemy);
-                PickTarget();
-            }
-        }
+				#region old code for targeting
+				//Debug.Log(visibleEnemies.Count);
 
-        Debug.Log(visibleEnemies.Count);
-    }
+				//if (Input.GetButtonDown("R3") && !switchButtonPressed)
+				//{
+				//	if (currentTarget == visibleEnemies[visibleEnemies.Count])
+				//		switchButtonPressed = true;
+				//	index++;
+				//	Debug.Log(visibleEnemies.Count);
+				//	if (index > visibleEnemies.Count)
+				//	{
+				//		index = 0;
+				//	}
+				//	currentTarget = visibleEnemies[index];
+				//}
+				//else if (Input.GetButtonUp("R3") && switchButtonPressed)
+				//{
+				//	switchButtonPressed = false;
+				//}
+				#endregion
+			}
+		}
+		CrosshairLock();
+	}
 
-    void CrosshairLock()
-    {
-        //spawns crosshair
-        Crosshair.SetActive(true);
-        Crosshair.transform.position = currentTarget.transform.position;
-    }
+	#region Finding Enemies
 
-    GameObject FindNearestVisibileEnemy()
-    {
-        //The nearest object (default if the first in the list)
-        GameObject nearestObject = (visibleEnemies.Count > 0) ? visibleEnemies[0].gameObject : null;
+	GameObject FindNearestVisibileEnemy()
+	{
+		//The nearest object (default if the first in the list)
+		GameObject nearestObject = (visibleEnemies.Count > 0) ? visibleEnemies[0].gameObject : null;
 
-        //If there is no nearest object, return null
-        if (!nearestObject)
-            return null;
+		//If there is no nearest object, return null
+		if (!nearestObject)
+			return null;
 
-        //Loop through all objects
-        for (int i = 0; i < visibleEnemies.Count; i++)
-        {
-            //If the object is the same as the current nearest object
-            if (visibleEnemies[i].gameObject == nearestObject)
-                //Continue to the next iteration
-                continue;
+		//Loop through all objects
+		for (int i = 0; i < visibleEnemies.Count; i++)
+		{
+			//If the object is the same as the current nearest object
+			if (visibleEnemies[i].gameObject == nearestObject)
+				//Continue to the next iteration
+				continue;
 
-            //The distance of the object and player
-            float distBetweenPlayer = (transform.position - visibleEnemies[i].transform.position).magnitude;
-            //The distance between the current nearest object and the player
-            float distBetweenNearest = (transform.position - nearestObject.transform.position).magnitude;
+			//The distance of the object and player
+			float distBetweenPlayer = (transform.position - visibleEnemies[i].transform.position).magnitude;
+			//The distance between the current nearest object and the player
+			float distBetweenNearest = (transform.position - nearestObject.transform.position).magnitude;
 
-            //If the distance of the object to the player is lesser than the 
-            //distance of the current nearest object to the player
-            if (distBetweenPlayer < distBetweenNearest)
-            {
-                //Set the current object to the nearest object.
-                nearestObject = visibleEnemies[i].gameObject;
-                index = i;
-            }
+			//If the distance of the object to the player is lesser than the 
+			//distance of the current nearest object to the player
+			if (distBetweenPlayer < distBetweenNearest)
+			{
+				//Set the current object to the nearest object.
+				nearestObject = visibleEnemies[i].gameObject;
+				selectedTarget = i;
+			}
 
-        }
+		}
+		return nearestObject;
+	}
 
-        return nearestObject;
-    }
+	GameObject FindNearestVisibileEnemy(List<GameObject> listOfEnemies)
+	{
+		//The nearest object (default if the first in the list)
+		GameObject nearestObject = (listOfEnemies.Count > 0) ? listOfEnemies[0].gameObject : null;
+
+		//If there is no nearest object, return null
+		if (!nearestObject)
+			return null;
+
+		//Loop through all objects
+		for (int i = 0; i < listOfEnemies.Count; i++)
+		{
+			//If the object is the same as the current nearest object
+			if (listOfEnemies[i].gameObject == nearestObject)
+				//Continue to the next iteration
+				continue;
+
+			//The distance of the object and player
+			float distBetweenPlayer = (transform.position - listOfEnemies[i].transform.position).magnitude;
+			//The distance between the current nearest object and the player
+			float distBetweenNearest = (transform.position - nearestObject.transform.position).magnitude;
+
+			//If the distance of the object to the player is lesser than the 
+			//distance of the current nearest object to the player
+			if (distBetweenPlayer < distBetweenNearest)
+			{
+				//Set the current object to the nearest object.
+				nearestObject = listOfEnemies[i].gameObject;
+			}
+
+		}
+		return nearestObject;
+	}
+
+	void FindAllEnemies()
+	{
+		State[] allEnemies = FindObjectsOfType(typeof(State)) as State[];
+		foreach (State Enemy in allEnemies)
+		{
+			//visibleEnemies.Add(Enemy);
+			this.allEnemies.Add(Enemy.gameObject);
+		}
+		this.allEnemies = this.allEnemies.Distinct().ToList();
+	}
+
+	void CheckForEnemiesInScreen()
+	{
+		visibleEnemies = new List<GameObject>();
+		foreach (GameObject Enemy in allEnemies)
+		{
+			Vector3 screenPoint = Camera.main.WorldToViewportPoint(Enemy.transform.position);
+			bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+			if (onScreen)
+			{
+				//CrosshairLock();
+				visibleEnemies.Add(Enemy);
+			}
+		}
+
+		Debug.Log(visibleEnemies.Count);
+	}
+
+	void SortListByDistance()
+	{
+		List<GameObject> visibleTargets = new List<GameObject>(visibleEnemies);
+		List<GameObject> sortedList = new List<GameObject>();
+		int index = 0;
+		while (visibleTargets.Count > 0)
+		{
+			GameObject nearestTarget = FindNearestVisibileEnemy(visibleTargets);
+			visibleTargets.Remove(nearestTarget);
+			sortedList.Add(nearestTarget);
+			index++;
+			if (index >= 10000)
+				UnityEditor.EditorApplication.isPlaying = false;
+		}
+		sortedVisibleEnemies = sortedList;
+	}
+
+	#endregion
+
+	void CrosshairLock()
+	{
+		//spawns crosshair
+		if (currentTarget)
+		{
+			Crosshair.SetActive(true);
+			Crosshair.transform.position = currentTarget.transform.position;
+		}
+	}
 }
