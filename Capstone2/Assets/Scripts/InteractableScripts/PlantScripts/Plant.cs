2d56acec5d74c4ab0d32a923edd38d849c4190eb
LAPTOP-END1ORH3\seank <seankeiffer@gmail.com>
2018-03-11 10:37:04 +0800

    + Added some untracked files

---
+++ Capstone2/Assets/Scripts/InteractableScripts/PlantScripts/Plant.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+
+	public bool IsActivated
+	{
+		get { return isActivated; }
+	}
+
+	private bool isActivated;
+	private bool hasSpawnedPlatform;
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!isActivated)
+			return;
+
+		if (!hasSpawnedPlatform)
+		{
+			//Spawn the plant here
+			Debug.Log("Im hit with water");
+		}
+
+	}
+
+	public void ActivatePlant()
+	{
+		isActivated = true;
+	}
+}
